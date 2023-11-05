1a35895ced400152f27d174a8605ca3b8ba6c3f0
Ing. Eduardo E. Enríquez R. <rekineke@hotmail.com>
2023-11-05 12:14:20 -0500

    integramos el menu hamburguesa con el menu de desktop y el menu del correo, y que se active o desactive haciendo clic en la imagen hamburguesa

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 //integramos la clase 11 y la 7, ahora voy a crear el código para que aparezca o desaparezca el menu de pequeño de navegación cuando haga clic en el correo electrónico en la parte superior derecha de la pantalla
 const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+//integramos la clase 8 a nuestro código, creamos varaibles para sufion
+const menuHamburguer = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navbarEmail.addEventListener("click", toggleDestopMenu);
+menuHamburguer.addEventListener("click", toggleMobileMenu);
 
 //Esta función hace visible nuestro pequeño menu a la derecha, cuando haga clic en el correo
 function toggleDestopMenu() {
-  if (window.getComputedStyle(desktopMenu).display === "none") {
-    desktopMenu.style.display = "block";
-  } else {
-    desktopMenu.style.display = "none";
-  }
-  //esta sería otra opcion que reemplaza el if, que coloca o quita la clase inactive
-  //desktopMenu.classList.toggle("inactive");
+  desktopMenu.classList.toggle("inactive");
+}
+
+//Esta función hace visible nuestro menu hamburguesa
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
 
