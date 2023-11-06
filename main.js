860132d0f8d03a4d2c78ca476d629f663224b938
Ing. Eduardo E. Enríquez R. <rekineke@hotmail.com>
2023-11-05 19:48:33 -0500

    Crando maquetación y agregando los productos desde JavaScript

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const mobileMenu = document.querySelector(".mobile-menu");
 //integramamos la clase 13 a nuestro código y creamos la fusión entre esta y la principal
 const navbarShoppingCart = document.querySelector(".navbar-shopping-cart");
 const asideProductDetail = document.querySelector(".product-detail");
+//integramos la clase 6 y creamos una variable para el contenedor de las tarjetas
+const cardsContainer = document.querySelector(".cards-container");
 
 navbarEmail.addEventListener("click", toggleDestopMenu);
 menuHamburguer.addEventListener("click", toggleMobileMenu);
@@ -42,6 +44,90 @@ function toggleAsideProductDetail() {
     mobileMenu.classList.add("inactive");
   }
   if (!isDesktopMenuClose) {
-    desktopMenu.classList.add("inactive");;
+    desktopMenu.classList.add("inactive");
   }
 }
+
+//Creamos código para agregar productos a la página y que se llene automáticamente, relacionado con la clase 6
+
+const productList = [];
+
+//Agregamos productos a la lista de productos
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Monopatineta",
+  price: 130,
+  image:
+    "https://http2.mlstatic.com/D_NQ_NP_912206-MCO47574546679_092021-O.webp",
+});
+
+productList.push({
+  name: "Patineta Eléctrica",
+  price: 180,
+  image:
+    "https://http2.mlstatic.com/D_NQ_NP_2X_967663-MLU71574173262_092023-F.webp",
+});
+
+productList.push({
+  name: "Triciclo",
+  price: 80,
+  image:
+    "https://http2.mlstatic.com/D_NQ_NP_820155-MLA48481276894_122021-O.webp",
+});
+
+productList.push({
+  name: "Carrito de Pedal",
+  price: 125,
+  image:
+    "https://http2.mlstatic.com/D_NQ_NP_769141-MCO52109910466_102022-O.webp",
+});
+
+//Recoriendo la lista de productos y creando un elemento por cada producto en la página Web
+function renderProductList(arr) {
+  for (product of arr) {
+  
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+  
+    const productImage = document.createElement("img");
+    productImage.src = product.image;
+    productImage.alt = product.name;
+  
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+  
+    const productDetails = document.createElement("div");
+  
+    const productName = document.createElement("p");
+    productName.textContent = product.name;
+  
+    const productPrice = document.createElement("p");
+    productPrice.textContent = `$${product.price.toFixed(2)}`;
+  
+    const addToCartFigure = document.createElement("figure");
+    const addToCartImage = document.createElement("img");
+    
+    addToCartImage.src = "./icons/bt_add_to_cart.svg";
+    addToCartImage.alt = "Add to cart";
+    addToCartFigure.appendChild(addToCartImage);
+  
+    productDetails.appendChild(productPrice);
+    productDetails.appendChild(productName);
+  
+    productInfo.appendChild(productDetails);
+    productInfo.appendChild(addToCartFigure);
+  
+    productCard.appendChild(productImage);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+renderProductList(productList);
